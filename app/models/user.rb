d26bfaf7a40a5a11cc816ae4f4d4fdc8fbde4d34
eloise <eloise.dube@gmail.com>
2015-05-27 15:31:24 -0400

    Ajouté unique sur attribut username et changé type du champ password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User
   include MongoMapper::Document
 
-  key :username, String, required: true
+  key :username, String, required: true, :unique => true
   key :password, String, required: true
   
   many :urls
