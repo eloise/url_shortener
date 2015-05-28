318d2566124dec7ed5f77d76743d18cc0cf08eaf
eloise <eloise.dube@gmail.com>
2015-05-27 23:24:35 -0400

    Ajouté la page d'accueil

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   resources :users
 
   resources :urls
+  
+  resource :welcome
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
