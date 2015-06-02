edce5bd5cddcddd1c0706b091aa54278bdcfe4dd
eloise <eloise.dube@gmail.com>
2015-06-02 13:18:47 -0400

    Ajouté la génération d'une url courte unique

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,8 @@
 class Url
   include MongoMapper::Document
+  
+  URL_BEGIN = "http://abc.ez/"
+  LENGTH = 7
 
   key :short, String
   key :long, String, required: true
@@ -7,5 +10,13 @@ class Url
   key :user_id, ObjectId
   
   belongs_to :user
-
+  
+  def self.shortener
+    begin
+      short = rand(36**LENGTH).to_s(36).ljust(LENGTH,'0')
+      unique = Url.where(short: short).count
+    end while unique != 0
+    short
+  end
+  
 end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UrlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    srand 1
+    @short_url = Url.shortener
+  end
+
+  test "generate short url with seven chars" do
+    assert /^[a-z|\d]{7}$/.match(@short_url)
+  end
+  
+  test "generate unique short url" do
+    FactoryGirl.create :url, short: @short_url
+    srand 1
+    assert_not_equal Url.shortener, @short_url
+  end
 end
